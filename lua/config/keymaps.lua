34d883fbc31f0b190e01c6c44cdb5a87c87b290a
kareulo-DEVGO-Studio <juab.jancarlo@devgo.studio>
2024-06-05 17:15:36 +0800

    feat: add new keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,16 @@ vim.keymap.set('n', '<Leader>q', vim.diagnostic.setloclist)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 
+vim.keymap.set({ 'n', 'x' }, '<Leader>D', [["+D]])
+vim.keymap.set({ 'n', 'x' }, '<Leader>d', [["+d]])
+
 vim.keymap.set({ 'n', 'x' }, '<Leader>P', [["+P]])
-vim.keymap.set({ 'n', 'x' }, '<Leader>Y', [["+Y]])
 vim.keymap.set({ 'n', 'x' }, '<Leader>p', [["+p]])
+
+vim.keymap.set({ 'n', 'x' }, '<Leader>Y', [["+Y]])
 vim.keymap.set({ 'n', 'x' }, '<Leader>y', [["+y]])
+
+if vim.opt.wrap then
+  vim.keymap.set('n', 'j', "v:count ? 'j' : 'gj'", { expr = true })
+  vim.keymap.set('n', 'k', "v:count ? 'k' : 'gk'", { expr = true })
+end
